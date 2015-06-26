e848571248744dd83ba6db344b1ca55b2533ab88
melena7 <elena.klueva@gmail.com>
2015-06-26 14:58:08 +0300

    Add: Third-party plugin section (MotoPress Slider)

---
+++ sections/third-party-plugins/en/__description.php
@@ -0,0 +1,2 @@
+<h2 class="item8"><i class="icon-magnet"></i>Third-party Plugins</h2>
+		<p></p>
---
+++ sections/third-party-plugins/en/motopress-slider.php
@@ -0,0 +1,134 @@
+<article id="motopress-slider">
+	<h3>MotoPress Slider Plugin</h3>
+	<p><a href="http://www.getmotopress.com/blog/responsive-motopress-slider-for-wordpress/" target="_blank">MotoPress Slider</a> is an easy to use and flexible solution for building compelling sliders with beautiful slides. It focuses on simplicity and functionality and offers all necessary tools to stand out among others. You can build your WordPress slider in two minutes and enjoy it from any size screen, as it looks great on any device. </p>
+	<p class="alert alert-warning">This instruction is for templates with MotoPress Slider included. If you purchased MotoPress Slider as an additional offer .json file will not be present in the template package. </p> 
+
+	<h4>To make MotoPress Slider look like our live demo, please follow the instructions below:</h4>
+	<ol>
+		<li>Install and activate MotoPress Slider plugin.</li>
+		<li>Go to <b>Dashboard -&gt; MotoPress Slider -&gt;</b> and press the button <b>"Import &amp; Export"</b>.</li>
+		<li>Select MotoPress Slider Export <b>.json</b> file that you can find in your template package.</li>
+		<li>After importing the settings of the slider, the installation is complete.</li>
+	</ol>
+
+	<h4>Slider settings:</h4>
+	<div class="row">
+		<div class="grid_6">
+			<figure class="img-polaroid">
+				<a href="img/cherryframework/third-party-plugins/motoslider1.jpg" class="pretty_image" rel="prettyPhoto[plugins]">
+					<img src="img/cherryframework/third-party-plugins/motoslider1.jpg" alt="" style="opacity: 1;"><i class="icon-search"></i><span></span>
+				</a>
+			</figure>
+		</div>
+		<div class="grid_6">
+			<ul>
+				<li><b>Slider title</b> - type the name of your slider, it should be easily recognizable in case you create several sliders for your website.</li>
+				<li><b>Slider alias</b> - helps the slider to provide you with the slider shortcode. Its name has to be unique.</li>
+				<li><b>Slider shortcode</b> - you'll insert this shortcode to the page or post where you want to place the slider.</li>
+				<li><b>Force Full Width</b> - enable this option to make this slider full-width.</li>
+				<li><b>Layers Grid Size</b> - enable to set width of your slider, indicate the layers grid size.</li>
+				<li><b>Enable Slideshow</b> - enable or disable slideshow.</li>
+				<li><b>Slideshow Delay</b> - the time one slide stays on the screen in milliseconds.</li>
+				<li><b>Pause on Hover</b> - Pause slideshow when hovering the slider.</li>
+				<li><b>Reverse order of the slides</b> - Animate slides in the reverse order.</li>
+				<li><b>Show counter</b> - Displays the number of slides.</li>
+			</ul>
+		</div>
+	</div>
+
+	<h4>Slide settings:</h4>
+	<p>After indicating the settings of the slider, it's time to start adding slides.<br/>
+	The process of customizing your slides is intuitive and simple. You just need to name your slide and select its background type: image or simply one color. </p>
+	<div class="row">
+		<div class="grid_6">
+			<figure class="img-polaroid">
+				<a href="img/cherryframework/third-party-plugins/motoslider2.jpg" class="pretty_image" rel="prettyPhoto[plugins]">
+					<img src="img/cherryframework/third-party-plugins/motoslider2.jpg" alt="" style="opacity: 1;"><i class="icon-search"></i><span></span>
+				</a>
+			</figure>
+		</div>
+		<div class="grid_6">
+			<h4>General:</h4>    
+			<ul>
+				<li><b>Slide Title</b> -  the title of the slide that will be shown in the slides list.</li>
+			    <li><b>Status</b> - choose status for slide (Published or Draft).</li>   
+			</ul> 
+			<h4>Color:</h4>    
+			<ul>
+				<li><b>Background color type:</b> - choose Color or Gradient for background of slide.</li>
+			    <li><b>Background Color</b> - please insert the color (this option is only for Background color type "Color").</li>
+			    <li><b>Gradient color 1, Gradient color 2, Gradient angle</b> -  insert two gradient colors and angle (these options are only for Background color type "Gradient").</li>
+			</ul> 
+			
+		</div>
+	</div>
+	<div class="row">
+		<div class="grid_6">
+			<h4>Image:</h4>    
+			<ul>
+				<li><b>Image Source</b> - image can be added from your computer, Media Library or External URL.</li>
+				<li><b>Size</b> - size of the slide image.
+			        <ul>
+						<li><b>Cover</b> - this means that the background image will be scaled as large as possible so the background area is fully covered with the image.</li>
+						<li><b>Contain</b> - the image can be scaled to the largest size. Both width and height fit to the content area.</li>
+						<li><b>Percentage</b> - the width and height of the background image can be fixed in percent.</li>
+						<li><b>Normal</b> -  the background image has its own width and height.</li>
+						<li>&nbsp;</li>
+					</ul>
+				</li>
+				<li><b>Repeat</b> - background image can be repeated or not.</li>
+				<li><b>Position</b> - choose the position of background image.</li>
+			</ul> 
+			<h4>Video:</h4>    
+			<ul>
+				<li><b>Video Source MP4</b> - upload .MP4 video file.</li>
+				<li><b>Video Source WEBM</b> - upload .WEBM video file.</li>
+				<li><b>Video Source OGG</b> - upload .OGG video file.</li>
+			    <li><b>Loop Video</b> - allows you to loop the video.</li>
+			    <li><b>Mute Video</b> - allows you to mute the video.</li>
+			    <li><b>Video Fill-mode</b> - choose Fill or Fit.</li>
+				<li><b>Video Cover</b> - enable Video Cover.
+			        <ul>
+						<li><b>Video Cover Type</b> -  choose Video Cover Type.</li>
+						<li>&nbsp;</li>
+					</ul>
+				</li>
+			</ul> 
+		</div>
+		<div class="grid_6">
+			<h4>Link:</h4>    
+			<ul>
+				<li><b>Link this slide</b> - insert link.</li>
+				<li><b>Open in new window</b> - open slide's link in new window.</li>
+				<li><b>Link id</b></li>
+			    <li><b>Link class</b></li>
+			    <li><b>Link rel</b></li>
+			    <li><b>Link title</b></li>    
+			</ul> 
+			<h4>Visibility:</h4>    
+			<ul>
+				<li><b>Only logged-in users can view this slide</b></li>
+				<li><b>Visible from</b> - choose the starting day from calender</li>
+				<li><b>Visible until</b> - choose the last day from calender</li>
+			</ul> 
+			<h4>Misc:</h4>    
+			<ul>
+				<li><b>Class name</b></li>
+				<li><b>CSS id</b></li>
+			</ul>
+		</div>
+	</div>
+	<p>The next step is to set the position of the background image and the 'repeat' option, but it depends on your wish only, as you can leave these fields empty.</p>
+	<p>For demonstration purposes, we'll use a few pictures in our slider to show you the advantages of the plugin. After setting the image options, you can get started with the Layer Settings.</p>
+	<img src="img/cherryframework/third-party-plugins/motoslider3.jpg" alt="" />
+	<p>This feature offers you a lot of customization options. You are able to create as many layers as you need. The layers can be presented as a text, image or button, for each layer you can set:</p>
+	<ul>
+	    <li><b>Start Animation</b> - how it'll appear on a slide.</li>
+		<li><b>End Animation</b> - the way how it will disappear from the slide.</li>
+		<li><b>Display</b> - the time it takes for one layer to appear.</li>
+		<li><b>Hide</b> - the time it takes for one layer to disappear. Note that time is calculated from the beginning of the slide and is indicated in milliseconds.</li>
+	</ul>
+	<p>Layer settings also enable to specify the text of the layer and its style. When you finish with the adding layers you can set their order simply by dragging and dropping them in any specific sequence.</p>
+	<p>For more information please click <a href="http://www.getmotopress.com/blog/responsive-motopress-slider-for-wordpress/" target="_blank">here</a>.</p>
+
+</article>
