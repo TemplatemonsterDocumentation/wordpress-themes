43019e890fef6def1fee28826a2e098a6a751e39
alexey-svistunov <alexeysvistunov@gmail.com>
2015-06-29 11:32:19 +0300

    UPD: requirements

--- sections/introduction/en/__description.php
+++ sections/introduction/en/__description.php
@@ -76,7 +76,42 @@
 					<p>As WordPress CMS is a PHP/MySQL platform, you need to have the hosting server prepared for it.</p>
 					<p>In case you already have a hosting server, you need to check whether it is compatibile with <a href="http://wordpress.org/about/requirements/" target="_blank"> WordPress hosting requirements </a>, in other words, whether you can host a WordPress website with it.</p>
 					
-					<p>If your hosting server meets WordPress requirements you should not have any difficulties with Cherry Framework 4.</p>
+					<p>Cherry Framework itself requires Apache or Nginx hosting servers with the following configuration settings:</p>
+					
+					<h5>Recommended Configuration</h5>
+
+					<ol>
+						<li>In <b>php.ini</b> define the following:<br>
+							<ul>
+								<li>'max_execution_time' => 60;</li>
+								<li>'memory_limit'  => 128;</li>
+								<li>'post_max_size'  => 8;</li>
+								<li>'upload_max_filesize'  => 8;</li>
+								<li>'max_input_time'  => 45;</li>
+								<li>'file_uploads'    => 'on';</li>
+								<li>'safe_mode'       => 'off';</li>
+							</ul>
+						</li>
+						<li>in <b>.htaccess</b> file: 'php_value max_execution_time' => 60;</li>
+						<li>in <b>wp-config.php</b>: 'set_time_limit' => 60;</li>
+						<li>1 GB of disk space</li>
+						<li>memory limit per process: 64mb (128mb or more recommended)</li>
+					</ol>
+					
+
+					<h5>PHP and MySQL</h5>
+					
+					<p>Minimal required version of PHP is 5.2.4 and MySQL 5. PHP 5.2 is already not safe as contains critical vulnerabilities that can be used to harm your website. Some Cherry Framework extensions will not work with PHP 5.2 and require version 5.4 or later.</p>
+
+					<p>Recommended settings are: </p>
+
+					<ol>
+						<li>PHP 5.4</li>
+						<li>MySQL 5.5 or later</li>
+						<li>mod_rewrite</li>
+						<li>php fopen</li>
+						<li>suPHP</li>
+					</ol>
 
 					<p>You can also install WordPress on your PC or laptop through a local server. You can use the next software to create a local server: <strong>WAMP</strong>, <strong>AppServ</strong>, <strong>MAMP</strong>, etc. All of these support WordPress and can be installed as regular software.</p>
 					<p>These tutorials will help you set up the local server:</p>
