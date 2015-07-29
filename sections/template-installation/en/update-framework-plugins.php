66ebcc3460bd1a9957d0f381eadbc5c4e1aa9d8e
melena7 <elena.klueva@gmail.com>
2015-07-29 15:37:31 +0300

    Add: update-framework-plugins

---
+++ sections/template-installation/en/update-framework-plugins.php
@@ -0,0 +1,8 @@
+<h3>How to update Cherry Framework and Cherry Plugins</h3>
+	<p>When new version of Cherry Framework is available you'll see a notification in your WordPress admin panel in "Dashboard -> Updates" section. </p>
+	<figure class="img-polaroid"><img src="img/cherryframework/update_cherry1.png" alt=""></figure>
+	<p>Click on 'updates' menu item to see what components can be updated. Just click on any element you want to update and proceed. The update process is exactly the same as WordPress core update. </p>
+
+	<p>Plugins, that have updates available can also be seen in "Plugins -> Update Available" section.</p>
+	<figure class="img-polaroid"><img src="img/cherryframework/update_plugins_2.png" alt=""></figure>
+	<p>Click "Update Now" button to download plugin updates.</p>
