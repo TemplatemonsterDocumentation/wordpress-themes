5c012188d7bc14b4624404dc9aee55f0d0cbdf99
melena7 <elena.klueva@gmail.com>
2015-08-27 12:27:48 +0300

    Add: replace Statics Editor into main menu

---
+++ sections/statics-editor/en/__description.php
@@ -0,0 +1,22 @@
+<h2>Statics Editor </h2>
+<p>Statics Editor is arrange static blocks (statics) in atatic areas. You can manage such blocks as: logo, header menu, header sidebar, search form, footer menu, footer info and footer sidebars.  </p>
+<figure class="img-polaroid">
+	<img src="img/cherryframework/static-editor-menu.jpg" alt="" >
+</figure>
+<p>Statics editor is available under Cherry admin panel menu item. Put your mouse cursor over "Cherry" menu item and in the appeared dropdown menu click "Statics Editor" link.</p>
+<figure class="img-polaroid">
+	<img src="img/cherryframework/static-editor.jpg" alt="">
+</figure>
+<p>With drag-n-drop interface you can move static item to the desired statis area. Clicking on the bucket icon you can remove static item from the area. Reomoved static items will still be available in 'Available statics' section. </p>
+
+<figure class="img-polaroid">
+	<img src="img/cherryframework/static-editor-settings.jpg" alt="" >
+</figure>
+<p>Each static item has configuration options. You can configure the grid settings for each static element and assign custom classes. Column class settings define the static item width according to <a href="http://getbootstrap.com/css/#grid-media-queries">Bootstrap 12 column markup grid</a>: </p>
+<ul>
+	<li><b>.col-xs-*</b>: - width for displays with horizontal screen resolution less than 768px</li>
+	<li><b>.col-sm-*</b>: - width for displays with horizontal screen resolution more or equal to 768px</li>
+	<li><b>.col-md-*</b>: - width for displays with horizontal screen resolution more or equal to 992px</li>
+	<li><b>.col-lg-*</b>: - width for displays with horizontal screen resolution more or equal to 1200px</li>
+</ul>
+<p>Also you can define item display conditions. Using conditions fields you can display statiс item for example only for logged in users and only at the home page (as displayed on the image).</p>
