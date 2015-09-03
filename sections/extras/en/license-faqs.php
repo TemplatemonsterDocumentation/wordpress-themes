78f8c39f9600edd2fd2ff0b7ec5c884da04f74d8
melena7 <elena.klueva@gmail.com>
2015-09-03 12:44:25 +0300

    Add: License FAQs

---
+++ sections/extras/en/license-faqs.php
@@ -0,0 +1,122 @@
+
+	<h3>License FAQs</h3>
+
+	<table class="table" cellspacing="10" cellpadding="10" border="1" width="100%">
+		<thead>
+			<th></th>
+			<th>ONE TIME USAGE LICENSE </th>
+			<th>+5 Licenses </th>
+			<th>+10 Licenses</th>
+		</thead>
+		<tr>
+			<th>Number of license keys delivered</th>
+			<td>1</td>
+			<td>5</td>
+			<td>10</td>
+		</tr>
+		<tr>
+			<th>Number of websites usage allowed</th>
+			<td>1 website</td>
+			<td>5 websites</td>
+			<td>10 websites</td>
+		</tr>
+		<tr>
+			<th>Number of sub-domain/local installations allowed</th>
+			<td>2</td>
+			<td>10</td>
+			<td>20</td>
+		</tr>
+		<tr>
+			<th>Use pre-packed PSD, images, elements, sample data etc for a website</th>
+			<td>Yes</td>
+			<td>Yes</td>
+			<td>Yes</td>
+		</tr>
+	</table>
+	<h4>ONE TIME USAGE LICENSE (regular price purchase)</h4>
+	<ol class="index-list">
+        <li>
+             <strong>What is a one time usage license?</strong><br>
+              When you buy the template at usual price you automatically get the one time usage license. It enables you to use each individual product on a single website only, belonging to either you or your client. You have to purchase the same template again if you plan to use the same design in connection with another or other projects. When passing files to your client please delete the template files from your computer.
+        </li>
+        <li>
+             <strong>Can I resell the template as is?</strong><br>
+              No, you can not resell the customized template as a template but you may sell it as a project to your client.
+        </li>
+        <li>
+             <strong>Can I customize the template?</strong><br>
+              Yes, you can customize the template to any extent needed. You can not use a header of the template for one project though, and images from the template for the other project.
+        </li>
+        <li>
+             <strong>Will you help me to customize my template?</strong><br>
+              24/7/365 Customer Support (via a live chat to get instant feedback). Address all kinds of your concerns to our professional support team any time. You will get a fast and clear solution to your issue, whether you need some technical help or smart advice on your website design. Over 100K Satisfied Users. We’ve taken into account wishes of each customer to improve our support service.
+        </li>
+        <li>
+             <strong>Are there any restrictions for using imagery, cliparts and fonts from the template I purchased?</strong><br>
+              All imagery, clipart and fonts used in our products are royalty-free and are the integral part of our products. One Time Usage License and Developers License give you the right to use images, clipart and fonts only as a part of the website you build using your template. You can use imagery, clipart and fonts to develop one project only. Any kind of separate usage or distribution is strictly prohibited.
+        </li>
+        <li>
+             <strong>I have customized the template and would like to protect my content by copyright, can I place Copyright 2008, Mycompanyname.com?</strong><br>
+              Yes, you can add your copyright to to protect your content.
+        </li>
+        <li>
+             <strong>Can I use graphics from the template we purchased for use in printing?</strong><br>
+              Yes, it’s not prohibited to use the template graphics for printed material but the resolution of the templates graphics ( except for logo templates) is not enough for printing. All the templates are designed for 72 dpi resolution while the printing requires 300 dpi resolution.
+        </li>
+        <li>
+             <strong>Can I get the template in higher resolution to use for printing?</strong><br>
+              No, we design templates optimized for web resolution only.
+        </li>
+        <li>
+             <strong>I like the image from the template and would like to use it for my advertising campaign, can you send me this image in resolution higher than 72 dpi?</strong><br>
+              No, we can’t provide image in higher size or resolution rather than it is on the template. We recommend looking for professional images on online photo resources.
+        </li>
+        <li>
+             <strong>Сan I use Monstroid and create a website for my client? </strong><br>
+              Yes, you can order the Monstroid theme, create a website for your customer and sell the modified website without any issues. 
+        </li>
+    </ol>
+    <h5>Plugins licenses:</h5>
+    <p>The licenses for most software are designed to take away your freedom to share and change it. By contrast, the GNU General Public License is intended to guarantee your freedom to share and change free software — to make sure the software is free for all its users. This General Public License applies to most of the Free Software Foundation’s software and to any other program whose authors commit to using it. (Some other Free Software Foundation software is covered by the GNU Library General Public License instead.) You can apply it to your programs, too.</p>
+    <p>The 100% GPL means others are free to deal with, sell or distribute the item, as long as they follow the requirements of the GPL. </p>
+
+    <ul class="marked-list">
+        <li>MotoPress Content Editor is released under the GPLv2 license</li>
+        <li>MotoPress Slider is released under the GPLv2 license</li>
+        <li>Cherry Framework plugins are released under the GPLv2 license</li>
+        <li>YITH WooCommerce Compare is released under the GPLv2 license</li>
+        <li>YITH WooCommerce Wishlist is released under the GPLv2 license</li>
+        <li>WooCommerce is released under the GPLv2 license</li>
+        <li>Regenerate Thumbnails  is released under the GPLv2 license</li>
+        <li>Monstroid Wizard is released under the GPLv2 license</li>
+        <li>MailChimp for WordPress Lite  is released under the GPLv2 license</li>
+        <li>Contact Form 7 is released under the GPLv2 license</li>
+    </ul>
+
+    <ol class="index-list">
+        <li>
+            <strong>Frequently Asked Questions</strong><br>
+            We recommend you to review our Frequently Asked Questions whether you’ve already purchased a template or just planning to do so.<br>
+            <a href="http://www.templatemonster.com/faq.php" target="_blank">Go to FAQ</a>
+        </li>
+        <li>
+            <strong>Terms of Use</strong><br>
+            Please read carefully this section because it contains the information on usage of our templates after the purchase.<br>
+            <a href="http://www.templatemonster.com/terms.php" target="_blank">Go to Terms of Use</a>
+        </li>
+        <li>
+            <strong>Refund Policy</strong><br>
+            You are strongly urged to review and acknowledge the conditions under which you may apply for a Refund/Exchange of the product you purchased from us. Every customer is obliged to agree with the terms of our Refund/Exchange policy upon placing an order with our company.<br>
+            <a href="http://www.templatemonster.com/refund-policy.php" target="_blank">Read Refund Policy</a>
+        </li>
+        <li>
+            <strong>Privacy Policy</strong><br>
+            Please read our privacy policy to learn more how we treat personally identifiable information collected from our visitors and users.<br>
+            <a href="http://www.templatemonster.com/privacy-policy.php" target="_blank">Read Privacy Policy</a>
+        </li>
+        <li>
+            <strong>Intellectual Property Rights Policy</strong><br>
+            Read Intellectual Property Rights Policy to learn more how we treat Intellectual Property Rights in our designs.<br>
+            <a href="http://www.templatemonster.com/intellectual-property.php" target="_blank">Read Intellectual Property Rights Policy</a>
+        </li>
+    </ol>
