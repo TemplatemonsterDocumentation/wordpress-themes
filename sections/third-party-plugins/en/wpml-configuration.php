0eee8de31d3409a5e0ba19abc20a85e95988a14e
melena7 <elena.klueva@gmail.com>
2015-09-03 13:12:37 +0300

    Add: referal links to wpml

--- sections/third-party-plugins/en/wpml-configuration.php
+++ sections/third-party-plugins/en/wpml-configuration.php
@@ -1,10 +1,10 @@
 <h2>WPML configuration</h2>
 
-<h5>WPML consists of several modules. The basic module turns WordPress sites multilingual. Other modules help with an enhanced translation process, string translation, media translation and other functions.</h5>
+<h5><a href="https://wpml.org/?aid=101280&affiliate_key=xc4iaSr81jkL" target="_blank">WPML</a> consists of several modules. The basic module turns WordPress sites multilingual. Other modules help with an enhanced translation process, string translation, media translation and other functions.</h5>
     
 <p>You can learn about the different modules in the <a href="http://wpml.org/documentation/wpml-core-and-add-on-plugins/" target="_blank"><strong>WPML Core and Addons</strong></a> page.</p>
     
-<p>After you enable WPML, you’ll see a Multilingual Widget in the Admin Dashboard. This widget provides access to the different functions in WPML. You can also access them through the <strong>WPML menu</strong>.</p>
+<p>After you enable <a href="https://wpml.org/?aid=101280&affiliate_key=xc4iaSr81jkL" target="_blank">WPML</a>, you’ll see a Multilingual Widget in the Admin Dashboard. This widget provides access to the different functions in <a href="https://wpml.org/?aid=101280&affiliate_key=xc4iaSr81jkL" target="_blank">WPML</a>. You can also access them through the <strong><a href="https://wpml.org/?aid=101280&affiliate_key=xc4iaSr81jkL" target="_blank">WPML</a> menu</strong>.</p>
 
 <ul class="marked-list">
 	<li><a href="http://wpml.org/documentation/getting-started-guide/language-setup/" target="_blank">Languages</a> – Determine the site’s languages and control how different language contents are organized.</li>
@@ -18,12 +18,12 @@
 
 <h4>Translating Site Content</h4>
 
-<p>WPML creates different posts, pages and custom types for translations and connects them together. This means that every page has its own title, body and meta-fields.</p>
+<p><a href="https://wpml.org/?aid=101280&affiliate_key=xc4iaSr81jkL" target="_blank">WPML</a> creates different posts, pages and custom types for translations and connects them together. This means that every page has its own title, body and meta-fields.</p>
     
-<p>WPML offers two ways for translating content:</p>
+<p><a href="https://wpml.org/?aid=101280&affiliate_key=xc4iaSr81jkL" target="_blank">WPML</a> offers two ways for translating content:</p>
 <ul class="marked-list">
 	<li>Manually, by clicking on the + icons</li>
-	<li>Using WPML’s Translation Management module</li>
+	<li>Using <a href="https://wpml.org/?aid=101280&affiliate_key=xc4iaSr81jkL" target="_blank">WPML</a>’s Translation Management module</li>
 </ul>
         
 <h4>Translation Dashboard</h4>
