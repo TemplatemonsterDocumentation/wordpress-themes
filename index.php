327c7a4c34518f6e6b186e850c0e66cdc5964ed2
melena7 <elena.klueva@gmail.com>
2016-08-09 14:47:45 +0300

    Add: GTM

--- index.php
+++ index.php
@@ -37,6 +37,33 @@ if (!isset($sections)) {
     <script src='<?php echo $path; ?>/js/device.min.js'></script>
 </head>
 <body data-section="<?php echo $section_param; ?>" onload="prettyPrint()">
+<?php
+$domain = $_SERVER['HTTP_HOST'];
+if(strpos($domain, 'templatemonster.com') !== false): ?>
+    <!--Templatemonster GTM -->
+    <!-- Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-MS2BNB"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MS2BNB');</script>
+<!-- End Google Tag Manager -->
+<?php elseif(strpos($domain, 'info.template-help.com') !== false): ?>
+    <!--TemplateHelp GTM-->
+    <!-- Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-WJX75J"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WJX75J');</script>
+<!-- End Google Tag Manager -->
+<?php else: ?>
+    <!-- Domain doesn't match. Google Tag Manager no added -->
+<?php endif; ?>
 <div class="page-wrap">
     <div class="rd-mobilemenu active">
         <div class="logo">
