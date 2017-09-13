c63cafd4ba0fe4d0838832fb21bc10ffeb25b25e
alex ross <alex.ross.th@gmail.com>
2017-09-13 12:55:37 +0300

    return '//china.fonts.url.com/';
    Signed-off-by: alex ross <alex.ross.th@gmail.com>

--- sections/extras/en/google-services.php
+++ sections/extras/en/google-services.php
@@ -8,5 +8,5 @@
 	<pre class="prettyprint">
 add_filter( 'cherry_google_fonts_cdn', 'cherry_china_fonts_url' );
 function cherry_china_fonts_url( $url ) {
-    return '//china.fonts.url.com/'
+    return '//china.fonts.url.com/';
 }</pre>
