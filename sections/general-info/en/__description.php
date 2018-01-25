42ee1620d2508cb575438cf9120750f9ce6d6fd4
alex ross <alex.ross.th@gmail.com>
2018-01-25 15:29:20 +0200

    SG offer
    Signed-off-by: alex ross <alex.ross.th@gmail.com>

--- sections/general-info/en/__description.php
+++ sections/general-info/en/__description.php
@@ -245,6 +245,14 @@
     </p>
 
     <p>Cherry Framework itself requires Apache or Nginx hosting servers with the following configuration settings:</p>
+	
+	<h4>Need a solid hosting provider? We recommend SiteGround!</h4>
+	
+	<p style="text-align:center;">
+		<a class="leadlink" target="_blank" href="https://goo.gl/VyS3yQ"><img src="https://www.templatemonster.com/help/wp-content/uploads/2018/01/SG_banner.png" alt="SiteGround Hosting">
+			</a>
+	</p>
+	<h5 style="text-align:center;"><strong> High performance platform suitable for projects of all sizes</strong></h5>
 
     <h5>Recommended Configuration</h5>
 
