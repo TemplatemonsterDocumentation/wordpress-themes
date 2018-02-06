58c35cb83045b47bee02268123701e9817e17426
alex ross <alex.ross.th@gmail.com>
2018-02-06 17:10:47 +0200

    remove SG
    Signed-off-by: alex ross <alex.ross.th@gmail.com>

--- sections/installation/en/__description.php
+++ sections/installation/en/__description.php
@@ -3,14 +3,6 @@
 
 <p class="alert alert-info custom">If you don't want to spend your time installing the template, don't hesitate to <span class="label label-warning opener" onclick="showChat()">order</span> installation service using our live-chat.</p>
 
-<h4>Need a solid hosting provider? We recommend SiteGround!</h4>
-	
-	<p style="text-align:center;">
-		<a class="leadlink" target="_blank" href="https://goo.gl/VyS3yQ"><img src="https://www.templatemonster.com/help/wp-content/uploads/2018/01/SG_banner.png" alt="SiteGround Hosting">
-			</a>
-	</p>
-	<h5 style="text-align:center;"><strong> High performance platform suitable for projects of all sizes</strong></h5>
-
 <p>All installation steps can be completed through the WordPress dashboard, which speeds up the whole process.</p>
 
 <p>You can verify this by clicking on the link below:</p>
